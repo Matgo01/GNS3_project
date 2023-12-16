59d65ccc73e6da1d15f80a0de6b264f7788dd6e4
matteo <gorlamatteo01@gmail.com>
2023-12-16 12:35:34 +0100

    aggiunta di nuovi filel

--- myRouter2/myDB.py
+++ myRouter2/myDB.py
@@ -29,19 +29,15 @@ def DB_connection():
     return db_connection
 
 def create_database_if_not_exists(db_name):
-
-
-    # Connessione al database 'postgres'
-    connection= DB_connection()
+    connection = DB_connection()
 
     try:
-        # Creazione di un cursore per eseguire comandi SQL
         with connection.cursor() as cursor:
             # Query SQL per verificare se il database esiste già
-            check_database_query = sql.SQL("SELECT 1 FROM pg_database WHERE datname = {}").format(sql.Identifier(db_name))
-
-            cursor.execute(check_database_query)
+            check_database_query = sql.SQL("SELECT 1 FROM pg_database WHERE datname = {}").format(sql.Placeholder())
+            cursor.execute(check_database_query, (db_name,))
             database_exists = cursor.fetchone()
+            print(f"Il database esiste")
 
             # Se il database non esiste, lo creiamo
             if not database_exists:
@@ -53,7 +49,6 @@ def create_database_if_not_exists(db_name):
         print(f"Errore durante la creazione del database: {e}")
 
     finally:
-        # Chiusura della connessione
         if connection:
             connection.close()
 
@@ -75,6 +70,7 @@ def create_table_if_not_exists():
         table_exists_query = sql.SQL("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = %s);")
         cursor.execute(table_exists_query, [table_name])
         table_exists = cursor.fetchone()[0]
+        print(f"La tabella esiste")
 
         # Se la tabella non esiste, la creiamo
         if not table_exists:
@@ -82,12 +78,12 @@ def create_table_if_not_exists():
          #Se columns_definition non è specificato, richiedilo da tastiera
          columns_definition = input("Inserisci la definizione delle colonne (es. col1 INT, col2 VARCHAR(255), ...): ")
 
-         create_table_query = sql.SQL("CREATE TABLE {} ({});").format(
-                sql.Identifier(table_name),
-             sql.SQL(', ').join(map(sql.Identifier, columns_definition.split(', ')))
-            )
+         create_table_query = sql.SQL("CREATE TABLE {} ({})").format(
+             sql.Identifier(table_name),
+             sql.SQL(columns_definition)
+         )
          cursor.execute(create_table_query)
-         print(f"La tabella {table_name} è stata creata con successo.")
+         print(f"Tabella '{table_name}' creata con successo.")
 
         # Commit delle modifiche
          connection.commit()
@@ -112,8 +108,13 @@ def insert_data_into_table(table_name, data):
         cursor = connection.cursor()
 
         # Inserimento dei dati nella tabella
-        insert_data_query = sql.SQL("INSERT INTO {} VALUES %s;").format(sql.Identifier(table_name))
-        cursor.execute(insert_data_query, [json.dumps(data)])
+        insert_data_query = sql.SQL("INSERT INTO {} VALUES (%s);").format(sql.Identifier(table_name))
+
+        # Converti la stringa JSON in un oggetto Python
+        data_json = json.dumps(data)
+
+        # Passa i valori come parte di una tupla
+        cursor.execute(insert_data_query, (data_json,))
 
         print(f"Dati inseriti con successo nella tabella {table_name}.")
 
